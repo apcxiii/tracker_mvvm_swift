7576210e03fb79cf5942a61911cf30bf137dcdfe
Erik Vera <apcxiii@gmail.com>
2019-11-26 13:35:51 -0600

    add test case for TrackDetailViewViewModel

--- TrackAppTests/TrackAppTests.swift
+++ TrackAppTests/TrackAppTests.swift
@@ -10,25 +10,33 @@ import XCTest
 @testable import TrackApp
 
 class TrackAppTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
+  var viewModel: TrackDetailViewViewModel!
+  
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    let trackRoute = TrackRoute()
+    trackRoute.name = "name"
+    trackRoute.duration = 0
+    trackRoute.timestamp = Date()
+    viewModel = TrackDetailViewViewModel(trackRoute: trackRoute)
+    
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testName(){
+    XCTAssertEqual(viewModel.name, "name")
+  }
+  
+  func testDuration() {
+    XCTAssertEqual(viewModel.duration, "0:00:00")
+  }
+  
+  func testTimeStamp() {
+    XCTAssertNotNil(viewModel.timestamp)
+  }
+  
 }
